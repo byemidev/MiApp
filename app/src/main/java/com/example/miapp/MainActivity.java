9807290bf3cfd790d541002c2b4213cccdb2cf39
byemidev <emilio.arevalozuniga@gmail.com>
2023-11-26 13:08:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/miapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_entrar;
+    EditText email, pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //mail, pass, btn_entrar
+        email = findViewById(R.id.editTextTextEmailAddress);
+        pass = findViewById(R.id.editTextTextPassword);
+        btn_entrar = (Button) findViewById(R.id.button_entrar);
+
+        //onClickListener()
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email_aux = email.getText().toString().toLowerCase().trim();
+                String pass_aux = pass.getText().toString().toLowerCase().trim();
+                if(!validarEmail(email_aux)){//validar email
+                    email.setError("email no valido");
+                }else{//si el email es valido..
+                    if(!validarContrasena(pass_aux)){//validar pass
+                        pass.setError("contraseña invalida");
+                        btn_entrar.setBackgroundColor(Color.RED);
+                    }else{
+                        btn_entrar.setBackgroundColor(Color.GREEN);
+                        switchToActivity_1();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean validarEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+
+    private boolean validarContrasena(String contrasena) {
+        return contrasena != null && contrasena.length() >= 6;
+    }
+
+    public void switchToActivity_1(){
+        Intent i = new Intent(this, Activity_1.class);
+        startActivity(i);
+    }
+}
