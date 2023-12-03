725566ad7302bc2fa07745ce9195bb390903d97f
byemidev <emilio.arevalozuniga@gmail.com>
2023-12-03 13:05:21 +0100

    terminado para primer parcial

--- app/src/main/java/com/example/miapp/Activity_1.java
+++ app/src/main/java/com/example/miapp/Activity_1.java
@@ -2,13 +2,35 @@ package com.example.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_1 extends AppCompatActivity {
-
+    private Button btn1, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+        btn1 = (Button) findViewById(R.id.btn_github);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/byemidev"));
+                startActivity(i1);
+            }
+        });
+
+        btn2= (Button) findViewById(R.id.btn_linkedin);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/emiarevalodev/"));
+                startActivity(i2);
+            }
+        });
     }
 }
