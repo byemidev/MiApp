2a6fd9b9ddcf2ea8e626dbf269045b50b865bb79
byemidev <emilio.arevalozuniga@gmial.com>
2023-12-14 19:29:44 +0100

    v_2 Adding buttons, ScrollView, Starting to do dowload cv.pdf

--- app/src/main/java/com/example/miapp/Activity_1.java
+++ app/src/main/java/com/example/miapp/Activity_1.java
@@ -5,8 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
 
 public class Activity_1 extends AppCompatActivity {
     private Button btn1, btn2;
@@ -32,5 +45,41 @@ public class Activity_1 extends AppCompatActivity {
                 startActivity(i2);
             }
         });
+    }
+
+    public void descargarPDF(){
+        String cv = "CV.pdf";
+        URL url = null;
+        try {
+            //Inicio Conexion
+            url = new URL("urlgithub/" + cv); //TODO
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.connect();
+            //
+            String Path = Environment.getExternalStorageDirectory() + "/download/";
+            Log.v("PdfManager", "PATH: " + Path);
+            File pdf = new File(Path);
+            //
+            pdf.mkdir();
+            if(pdf.exists()){
+                try{
+                    FileOutputStream fos = new FileOutputStream(pdf);
+                    InputStream is = connection.getInputStream();
+                    //TODO
+                    Byte [] buffer = new Byte[1024];
+                    // While()...
+                    //Closing..
+                }catch (FileNotFoundException e){
+                    System.err.printf( "Error en descargarPDF--> \n %d", e.getCause());
+                }
+            }
+            connection.disconnect();//Nos podemos desconectar una vez se ha realizado la conexion
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
